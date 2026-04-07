672bbfd41b9acd4bd48f740c154e44c92a78db10
Myles Lewis Page <68628500+Myles-Lewis-Page@users.noreply.github.com>
2026-04-06 19:28:58 -0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// ============================================================
-// SUPABASE CONFIGURATION
-// Replace these two values with your own from:
-//   Supabase Dashboard → Project Settings → API
-// ============================================================
-
-const SUPABASE_URL  = 'https://kazkjnscimldtfzrekkf.supabase.co';
-const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImthemtqbnNjaW1sZHRmenJla2tmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1MTg0OTAsImV4cCI6MjA5MTA5NDQ5MH0.gLEdnlhVUdFpzGNdF2K1u_AUFfItgeOJxe_yIhr7bJU';
